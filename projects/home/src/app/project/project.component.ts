e762bfc7bbf68b0fd1e4d955792e9b3e5c3ade93
amged123ahmedy <159439917+amged123ahmedy@users.noreply.github.com>
2025-04-30 14:27:52 +0300

    Your commit message

---
+++ projects/home/src/app/project/project.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'project-root',
+  standalone: true,
+  template: `<h1>Hello from Project Component!</h1>`,
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent {}
