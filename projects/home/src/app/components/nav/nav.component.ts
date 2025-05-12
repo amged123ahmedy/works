c1cabef55c90cae684cc1e183a234e92f31ae17d
amged123ahmedy <159439917+amged123ahmedy@users.noreply.github.com>
2025-05-12 16:41:23 +0300

    تعديلات على الـ nav وملفات التنسيق

--- projects/home/src/app/components/nav/nav.component.ts
+++ projects/home/src/app/components/nav/nav.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
-import { CommonModule } from '@angular/common';
+
+type DropdownType = 'projects' | 'pages' | 'menu';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterModule, NgIf], // <-- Add NgIf here
+  imports: [RouterModule, NgIf],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-  dropdownOpen = {
+  dropdownOpen: Record<DropdownType, boolean> = {
     projects: false,
     pages: false,
     menu: false
   };
 
-  toggleDropdownMenu(menu: 'projects' | 'pages' | 'menu') {
+  toggleDropdownMenu(menu: DropdownType) {
     if (menu === 'menu') {
       this.dropdownOpen.menu = !this.dropdownOpen.menu;
       if (!this.dropdownOpen.menu) {
